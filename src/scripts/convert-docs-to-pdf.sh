a3ecb990e8455e284fe74f0c6e3d870d9493485e
CodeForFaith <codeforfaith@gmail.com>
2021-01-26 19:36:16 +0100

    update comments

--- src/scripts/convert-docs-to-pdf.sh
+++ src/scripts/convert-docs-to-pdf.sh
@@ -3,3 +3,6 @@
 # PDF support: sudo apt intall texlive
 # missing letltxmacro.sty: sudo apt install texlive-latex-extra
 # Demo: pandoc vocabulary.md -o vocabulary.pdf
+# Demo with CSS: pandoc vocabulary.md -t html -c github-markdown.css -o vocabulary.pdf
+# Stylesheet: https://gist.github.com/tuzz/3331384
+# Docs: https://pandoc.org/MANUAL.html "Creating PDF"
