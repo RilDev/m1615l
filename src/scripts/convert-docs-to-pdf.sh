1f579e5fa4fff38988f6f69dfada04664ec3addc
RilDev <c.public@tutanota.com>
2021-01-26 15:28:15 +0100

    add pdf convert script

---
+++ src/scripts/convert-docs-to-pdf.sh
@@ -0,0 +1,5 @@
+# Install Pandoc:
+# DEB package: https://github.com/jgm/pandoc/releases/tag/2.11.4
+# PDF support: sudo apt intall texlive
+# missing letltxmacro.sty: sudo apt install texlive-latex-extra
+# Demo: pandoc vocabulary.md -o vocabulary.pdf
